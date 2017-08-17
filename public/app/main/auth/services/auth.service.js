9144ae9c6583e174f5507545918a6fb03c121859
inms_adarsh <adarshsomani64@gmail.com>
2017-08-17 23:26:31 +0530

    add new users

--- public/app/main/auth/services/auth.service.js
+++ public/app/main/auth/services/auth.service.js
@@ -1,4 +1,4 @@
-(function() {
+(function () {
     'use strict';
 
     angular
@@ -38,11 +38,11 @@
             return defer.promise;
         }
 
-         /**
-         * get Current Tenant Id
-         */
+        /**
+        * get Current Tenant Id
+        */
         function getCurrentTenant(data) {
-             return localStorage.getItem('tenantId') ? localStorage.getItem('tenantId').replace(/["']/g, ""):null;
+            return localStorage.getItem('tenantId') ? localStorage.getItem('tenantId').replace(/["']/g, "") : null;
         }
 
         /**
@@ -78,9 +78,9 @@
         function registerUser(user) {
             var def = $q.defer();
 
-            auth.$createUserWithEmailAndPassword(user.email, user.password).then(function(data) {
+            auth.$createUserWithEmailAndPassword(user.email, user.password).then(function (data) {
                 def.resolve(data);
-            }).catch(function(err) {
+            }).catch(function (err) {
                 def.reject(err);
             });
             return def.promise;
@@ -101,8 +101,8 @@
                     uid: authData.uid
                 };
 
-            if (tenantId) {
-                userData.tenantId = tenantId;
+            if (getCurrentTenant()) {
+                userData.tenantId = getCurrentTenant();
             }
             return firebaseUtils.addData(userObj, userData);
         }
@@ -114,10 +114,15 @@
         function addTenant(tenant) {
             var tenantObj = rootRef.child('tenants'),
                 def = $q.defer();
-            
-            delete tenant.password;
 
-            return firebaseUtils.addData(tenantObj, tenant);        }
+            delete tenant.password;
+            if (getCurrentTenant()) {
+                def.resolve(getCurrentTenant());
+                return def.promise;
+            } else {
+                return firebaseUtils.addData(tenantObj, tenant);
+            }
+        }
 
         /**
          * Add new tenantId
@@ -132,7 +137,7 @@
                 };
             mergeObj['users/' + uid + '/tenantId'] = tenantId;
             mergeObj['tenant-users/' + tenantId + '/' + uid] = userObj;
-            mergeObj['users-uid/'+ authData.uid + '/tenantId'] = tenantId;
+            mergeObj['users-uid/' + authData.uid + '/tenantId'] = tenantId;
             return firebaseUtils.updateData(rootRef, mergeObj);
         }
         /**
@@ -140,7 +145,7 @@
          * @param {String} tenant Id
          */
         function retrieveTenant(tenantId) {
-            if(!tenantId) {
+            if (!tenantId) {
                 tenantId = this.getCurrentTenant();
             }
             var ref = rootRef.child('tenants').child(tenantId);
@@ -152,7 +157,7 @@
          * @param {Object} tenant Information object
          */
         function updateTenantInfo(tenant, tenantId) {
-             if(!tenantId) {
+            if (!tenantId) {
                 tenantId = this.getCurrentTenant();
             }
             var ref = rootRef.child('tenants').child(tenantId);
@@ -174,10 +179,10 @@
          */
         function updatePassword(passwordObj) {
             var def = $q.defer();
-            auth.$updatePassword(passwordObj.password, function(response) {
-               def.resolve(response);
-            }, function(error) {
-              def.reject(errot);
+            auth.$updatePassword(passwordObj.password, function (response) {
+                def.resolve(response);
+            }, function (error) {
+                def.reject(errot);
             });
 
             return def.promise;
@@ -189,24 +194,24 @@
         function forgotPassword(email) {
             var def = $q.defer();
 
-            auth.$sendPasswordResetEmail(email).then(function() {
-                  console.log("Password reset email sent successfully!");
-            }).catch(function(error) {
-                  console.error("Error: ", error);
-             });
+            auth.$sendPasswordResetEmail(email).then(function () {
+                console.log("Password reset email sent successfully!");
+            }).catch(function (error) {
+                console.error("Error: ", error);
+            });
         }
 
         /**
          * Create tenant
          */
         function redirect(user, profile, authData) {
-            addTenant(user).then(function(key){
-                updateUserTenantId(profile, key, user, authData).then(function(){
-                    setCurrentTenant(key).then(function(){
+            addTenant(user).then(function (key) {
+                updateUserTenantId(profile, key, user, authData).then(function () {
+                    setCurrentTenant(key).then(function () {
                         $state.go('app.records.list');
                     });
                 });
-            }).catch(function(err){
+            }).catch(function (err) {
 
             });
         }
--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5149,7 +5149,7 @@
         
     }
 })();
-(function() {
+(function () {
     'use strict';
 
     authService.$inject = ["$firebaseArray", "$firebaseObject", "auth", "$q", "$timeout", "$state", "firebaseUtils"];
@@ -5190,11 +5190,11 @@
             return defer.promise;
         }
 
-         /**
-         * get Current Tenant Id
-         */
+        /**
+        * get Current Tenant Id
+        */
         function getCurrentTenant(data) {
-             return localStorage.getItem('tenantId') ? localStorage.getItem('tenantId').replace(/["']/g, ""):null;
+            return localStorage.getItem('tenantId') ? localStorage.getItem('tenantId').replace(/["']/g, "") : null;
         }
 
         /**
@@ -5230,9 +5230,9 @@
         function registerUser(user) {
             var def = $q.defer();
 
-            auth.$createUserWithEmailAndPassword(user.email, user.password).then(function(data) {
+            auth.$createUserWithEmailAndPassword(user.email, user.password).then(function (data) {
                 def.resolve(data);
-            }).catch(function(err) {
+            }).catch(function (err) {
                 def.reject(err);
             });
             return def.promise;
@@ -5253,8 +5253,8 @@
                     uid: authData.uid
                 };
 
-            if (tenantId) {
-                userData.tenantId = tenantId;
+            if (getCurrentTenant()) {
+                userData.tenantId = getCurrentTenant();
             }
             return firebaseUtils.addData(userObj, userData);
         }
@@ -5266,10 +5266,15 @@
         function addTenant(tenant) {
             var tenantObj = rootRef.child('tenants'),
                 def = $q.defer();
-            
-            delete tenant.password;
 
-            return firebaseUtils.addData(tenantObj, tenant);        }
+            delete tenant.password;
+            if (getCurrentTenant()) {
+                def.resolve(getCurrentTenant());
+                return def.promise;
+            } else {
+                return firebaseUtils.addData(tenantObj, tenant);
+            }
+        }
 
         /**
          * Add new tenantId
@@ -5284,7 +5289,7 @@
                 };
             mergeObj['users/' + uid + '/tenantId'] = tenantId;
             mergeObj['tenant-users/' + tenantId + '/' + uid] = userObj;
-            mergeObj['users-uid/'+ authData.uid + '/tenantId'] = tenantId;
+            mergeObj['users-uid/' + authData.uid + '/tenantId'] = tenantId;
             return firebaseUtils.updateData(rootRef, mergeObj);
         }
         /**
@@ -5292,7 +5297,7 @@
          * @param {String} tenant Id
          */
         function retrieveTenant(tenantId) {
-            if(!tenantId) {
+            if (!tenantId) {
                 tenantId = this.getCurrentTenant();
             }
             var ref = rootRef.child('tenants').child(tenantId);
@@ -5304,7 +5309,7 @@
          * @param {Object} tenant Information object
          */
         function updateTenantInfo(tenant, tenantId) {
-             if(!tenantId) {
+            if (!tenantId) {
                 tenantId = this.getCurrentTenant();
             }
             var ref = rootRef.child('tenants').child(tenantId);
@@ -5326,10 +5331,10 @@
          */
         function updatePassword(passwordObj) {
             var def = $q.defer();
-            auth.$updatePassword(passwordObj.password, function(response) {
-               def.resolve(response);
-            }, function(error) {
-              def.reject(errot);
+            auth.$updatePassword(passwordObj.password, function (response) {
+                def.resolve(response);
+            }, function (error) {
+                def.reject(errot);
             });
 
             return def.promise;
@@ -5341,24 +5346,24 @@
         function forgotPassword(email) {
             var def = $q.defer();
 
-            auth.$sendPasswordResetEmail(email).then(function() {
-                  console.log("Password reset email sent successfully!");
-            }).catch(function(error) {
-                  console.error("Error: ", error);
-             });
+            auth.$sendPasswordResetEmail(email).then(function () {
+                console.log("Password reset email sent successfully!");
+            }).catch(function (error) {
+                console.error("Error: ", error);
+            });
         }
 
         /**
          * Create tenant
          */
         function redirect(user, profile, authData) {
-            addTenant(user).then(function(key){
-                updateUserTenantId(profile, key, user, authData).then(function(){
-                    setCurrentTenant(key).then(function(){
+            addTenant(user).then(function (key) {
+                updateUserTenantId(profile, key, user, authData).then(function () {
+                    setCurrentTenant(key).then(function () {
                         $state.go('app.records.list');
                     });
                 });
-            }).catch(function(err){
+            }).catch(function (err) {
 
             });
         }
